cc91aa319773d533377d8a382d84edc602fb648b
GezahegnTsegaye <gezut38@gmail.com>
2020-05-10 23:55:13 +0200

    update on admin services

--- src/main/java/com/sample/tms/services/impl/AdministratorServiceImpl.java
+++ src/main/java/com/sample/tms/services/impl/AdministratorServiceImpl.java
@@ -1,11 +1,21 @@
 package com.sample.tms.services.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.sample.tms.entity.Administrator;
+import com.sample.tms.repository.AdministratorRepository;
 import com.sample.tms.services.AdministratorService;
 
 @Service
 public class AdministratorServiceImpl implements AdministratorService{
+	
+	
+	@Autowired
+	private AdministratorRepository administratorRepository;
+	
+	
 	
 	
 	
