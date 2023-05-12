904d29c267ae17488a0d19e04d1d6f538f55ce09
GezahegnTsegaye <gezut38@gmail.com>
2023-05-12 14:05:02 +0200

    Created Repository class

---
+++ evaluator/src/main/java/com/tms/dal/repository/TenderOfferRepository.java
@@ -0,0 +1,7 @@
+package com.tms.dal.repository;
+
+import com.tms.dal.model.TenderOffer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TenderOfferRepository extends JpaRepository<TenderOffer, Long> {
+}
--- tenderer/src/main/java/com/tms/dal/repository/BusinessRegistrationRepository.java
+++ tenderer/src/main/java/com/tms/dal/repository/BusinessRegistrationRepository.java
@@ -1,6 +1,6 @@
 package com.tms.dal.repository;
 
-import com.tms.dal.models.BusinessRegistration;
+import com.tms.dal.models.business.BusinessRegistration;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BusinessRegistrationRepository extends JpaRepository<BusinessRegistration, Long> {
