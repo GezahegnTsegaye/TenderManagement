79d13ccca3578f4e2925c09b519340eef71be5b8
Gezahegn Tsegaye <gezut38@gmail.com>
2025-03-08 17:51:14 +0100

    update discovery services structure

--- discovery-service/src/main/java/com/egov/tendering/discovery/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/com/egov/tendering/discovery/DiscoveryServiceApplication.java
@@ -1,18 +1,14 @@
-package com.tms;
+package com.egov.tendering.discovery;
 
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class DiscoveryServerApplication {
-
-
+public class DiscoveryServiceApplication {
     public static void main(String[] args) {
-        new SpringApplicationBuilder(DiscoveryServerApplication.class).run(args);
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
     }
-
-
 }
