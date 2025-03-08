5d306b88838aae30bfebd8d83fd418ca559d5f01
Gezahegn Tsegaye <gezut38@gmail.com>
2025-03-09 00:02:07 +0100

    modified services

--- tenderer/src/main/java/com/tms/dal/repository/BidRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tms.dal.repository;
-
-import com.tms.dal.models.bid.Bid;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BidRepository extends JpaRepository<Bid, Long> {
-}
