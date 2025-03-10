d63b92bf8257db829fb1e8a59925cb8c30971b0d
Gezahegn Tsegaye <gezut38@gmail.com>
2025-03-10 09:31:21 +0100

    added model classes for document service

---
+++ document-service/src/main/java/com/egov/tendering/document/event/DocumentEventType.java
@@ -0,0 +1,14 @@
+package com.egov.tendering.document.event;
+
+/**
+ * Types of document events
+ */
+public enum DocumentEventType {
+    UPLOADED,
+    UPDATED,
+    DOWNLOADED,
+    DELETED,
+    EXPIRED,
+    VERIFIED,
+    REJECTED
+}
